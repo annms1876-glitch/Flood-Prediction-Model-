f333cd315fee1b675ecc5e0b313b72bf1c3e87d0
annms1876-glitch <annms1876-glitch@users.noreply.github.com>
2026-09-07 17:03:28 +0530

    fix: remove babel-loader webpack config, use SWC default

--- floodlens-ui/next.config.js
+++ floodlens-ui/next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   output: "standalone",
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_ML_URL: process.env.NEXT_PUBLIC_ML_URL || "http://localhost:8000",
+    NEXT_PUBLIC_ML_URL: process.env.NEXT_PUBLIC_ML_URL || "http://flood-prediction-model.railway.internal",
     NEXT_PUBLIC_BACKEND_URL:
-      process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3000",
+      process.env.NEXT_PUBLIC_BACKEND_URL || "http://umeedai.railway.internal",
   },
   async rewrites() {
     return [
@@ -18,23 +18,6 @@ const nextConfig = {
         destination: `${process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3000"}/:path*`,
       },
     ];
-  },
-  webpack: function (config, { isServer }) {
-    config.module.rules.push({
-      test: /\.[jt]sx?$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader",
-        options: {
-          presets: [
-            "@babel/preset-env",
-            ["@babel/preset-react", { runtime: "automatic" }],
-            "@babel/preset-typescript",
-          ],
-        },
-      },
-    });
-    return config;
   },
 };
 
